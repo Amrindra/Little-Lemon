d05aa32e4e00dd8497f16d9bb8b00ca2021868fd
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-02-24 21:03:57 -0500

    Added meta tags to the <head> in the index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Footer from "./component/footer/Footer";
+import Header from "./component/header/Header";
+import Main from "./component/main/Main";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome to the little lemon</h1>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
